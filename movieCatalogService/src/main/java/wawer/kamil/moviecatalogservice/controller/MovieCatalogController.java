e8bcccee16f380946ef3ed427e2fd3ca10fef50d
Martin056PL <kamil.wawer@pollub.edu.pl>
2020-05-20 18:08:55 +0200

    Add RestTemplate bean, add models

--- movieCatalogService/src/main/java/wawer/kamil/moviecatalogservice/controller/MovieCatalogController.java
+++ movieCatalogService/src/main/java/wawer/kamil/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,22 +1,40 @@
 package wawer.kamil.moviecatalogservice.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import wawer.kamil.moviecatalogservice.model.CatalogItem;
+import wawer.kamil.moviecatalogservice.model.Movie;
+import wawer.kamil.moviecatalogservice.model.Rating;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
+@RequiredArgsConstructor
 public class MovieCatalogController {
 
+    private final RestTemplate template;
+
     @GetMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") Long userId){
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "description for Transormers", 4));
+    public List<CatalogItem> getCatalog(@PathVariable("userId") Long userId) {
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating(1L, 4), new Rating(3L, 3)
+        );
+
+        return ratings.stream()
+                .map(rating -> {
+                    Movie movie = template.getForObject("http://localhost:8081/movies/"+  rating.getId(), Movie.class);
+                    return new CatalogItem(movie.getTitle(), "Description", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
     }
 
 }
---
+++ movieCatalogService/src/main/java/wawer/kamil/moviecatalogservice/model/Movie.java
@@ -0,0 +1,15 @@
+package wawer.kamil.moviecatalogservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    private Long id;
+    private String title;
+
+}
