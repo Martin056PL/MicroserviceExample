68c9081ccdfa920f734f74867dc4b0afd873cdbf
Martin056PL <kamil.wawer@pollub.edu.pl>
2020-05-20 14:40:02 +0200

    Add clean projects

---
+++ movieCatalogService/src/test/java/wawer/kamil/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wawer.kamil.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
